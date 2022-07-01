bf7170ff74286a31c4a6b15922e16ad971ec6618
drummermanrob20 <robert@stapleton-family.net>
2022-07-01 14:40:07 -0400

    params

--- Recursion/Grid.Traveler.Memoization.ps1
+++ Recursion/Grid.Traveler.Memoization.ps1
@@ -2,5 +2,14 @@
 Say you are a traveler on a 2D grid.  You begin in the top-left corner and your goal is to travel to the bottom-right corner.
 You may only move down or right.
 
-In how many ways can you travel to the goal on a grid with dimensions r * n?
+In how many ways can you travel to the goal on a grid with dimensions r * c?
 #>
+
+function GridTraveler {
+    param (
+        [Parameter(Mandatory)]    
+        [int]$r,
+        [Parameter(Mandatory)]
+        [int]$c
+    )
+}
