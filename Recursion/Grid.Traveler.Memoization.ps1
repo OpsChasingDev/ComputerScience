e32c8ec43bb012f38b1220886f43e2a42c47a5c0
drummermanrob20 <robert@stapleton-family.net>
2022-07-01 15:32:02 -0400

    new logic

--- Recursion/Grid.Traveler.Memoization.ps1
+++ Recursion/Grid.Traveler.Memoization.ps1
@@ -5,7 +5,11 @@ You may only move down or right.
 In how many ways can you travel to the goal on a grid with dimensions r * c?
 #>
 
-
+function GridTraveler ([int]$r, [int]$c) {
+    if ($r -eq 0 -or $c -eq 0) {return 0}
+    if ($r -eq 1 -and $c -eq 1) {return 1}
+    GridTraveler($r, $c - 1) + GridTraveler ($r - 1, $c)
+}
 
 <#
 function GridTraveler {
