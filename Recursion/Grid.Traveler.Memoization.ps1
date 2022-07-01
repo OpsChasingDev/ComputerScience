3c7f8bc033d43899db6b27debfdce5b2e79db56a
drummermanrob20 <robert@stapleton-family.net>
2022-07-01 15:06:23 -0400

    positional params

--- Recursion/Grid.Traveler.Memoization.ps1
+++ Recursion/Grid.Traveler.Memoization.ps1
@@ -7,14 +7,17 @@ In how many ways can you travel to the goal on a grid with dimensions r * c?
 
 function GridTraveler {
     param (
-        [Parameter(Mandatory)]    
+        [Parameter(Mandatory, Position=0)]    
         [int]$r,
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, Position=1)]
         [int]$c
     )
     # base cases
     if ($r -eq 0 -or $c -eq 0) {Write-Output 0}
     if ($r -eq 1 -and $c -eq 1) {Write-Output 1}
     # recursion logic
-    Write-Output {(GridTraveler -r ($r - 1) -c $c) + (GridTraveler -r $r -c ($c - 1))}
+    $RowMinus = GridTraveler -r ($r - 1) -c $c
+    $ColumnMinus = GridTraveler -r $r -c ($c - 1)
+    $Output = $RowMinus + $ColumnMinus
+    Write-Output $Output
 }
