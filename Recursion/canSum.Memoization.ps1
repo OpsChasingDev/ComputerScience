9a0af740bfa89ef487f616e19557dc1d5355c8b0
drummermanrob20 <robert@stapleton-family.net>
2022-07-14 10:59:59 -0400

    added examples

--- Recursion/canSum.Memoization.ps1
+++ Recursion/canSum.Memoization.ps1
@@ -5,3 +5,10 @@
 - members of the array can be used as many times as needed
 - all input numbers are positive
 #>
+
+<#
+Examples:
+canSum(10, [2,3,4]) -> true
+canSum(10, [3,4]) -> true
+canSum(10, [4,7]) -> false
+#>
