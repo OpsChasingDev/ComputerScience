6e23a71ba56186424cf9f8e88aa4d8050baa5136
drummermanrob20 <robert@stapleton-family.net>
2022-07-14 10:50:35 -0400

    problem details

---
+++ Recursion/canSum.Memoization.ps1
@@ -0,0 +1,7 @@
+<#
+- write a function that determines whether an array of numbers can be used to read a target sum
+- input will be a target sum to reach and an array of numbers with which the sum is reached
+- output should be true or false, meaning either the target sum can or cannot be reached using the array members
+- members of the array can be used as many times as needed
+- all input numbers are positive
+#>
