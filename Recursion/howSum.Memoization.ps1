511b08a964895af9fa17af6a368c486b240b6ff7
drummermanrob20 <robert@stapleton-family.net>
2022-07-19 07:52:41 -0400

    program rules

--- Recursion/howSum.Memoization.ps1
+++ Recursion/howSum.Memoization.ps1
@@ -1,1 +1,7 @@
-howSum.Memoization.ps1
+<#
+- write a function that returns a combination of numbers from an array set that adds up to a target number
+- input will be a targetSum to add up to and an array of numbers that can be used as options to reach that targetSum
+- output will be the combination of numbers used to add up to the targetSum
+- if multiple options exist, only one needs to be returned
+- if no options exist, return null
+#>
