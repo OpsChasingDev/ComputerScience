158d5e4190397e883477ee1ab79b36d5f33fd78b
drummermanrob20 <robert@stapleton-family.net>
2022-07-19 08:07:06 -0400

    return null after adding collection member to results array

--- Recursion/howSum.Memoization.ps1
+++ Recursion/howSum.Memoization.ps1
@@ -20,7 +20,11 @@ function howSum {
         # if the TargetSum is less than the member of the collection, return null
         if ($TargetSum -lt $c) { return $null }
         # if the TargetSum is equal to the member of the collection, add collection member into an array of results
-        if ($TargetSum -eq $c) { $Results += $c }
+        if ($TargetSum -eq $c) {
+            $Results += $c
+            return $null
+        }
+
     }
     # if the TargetSum is greater than the member of the collection, work the logic
         # subtract the collection member from the current TargetSum and use that value as the new input for TargetSum
