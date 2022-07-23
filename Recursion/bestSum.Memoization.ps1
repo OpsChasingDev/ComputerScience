89273331c2524940d43db15e83be4a1a0cb1092e
drummermanrob20 <robert@stapleton-family.net>
2022-07-23 08:09:28 -0400

    rules in comment

---
+++ Recursion/bestSum.Memoization.ps1
@@ -0,0 +1,7 @@
+<#
+    - given a target sum and a set of numbers in an array, find the smallest set of elements in the array that add up to the target sum
+    - input will consist of a target sum and a collection of numbers to choose from for the addition
+    - output will be the smallest set of elements from the input collection to reach the target sum
+    - elements in the array can be used more than once
+    - a tie between sizes of array results can return either option
+#>
